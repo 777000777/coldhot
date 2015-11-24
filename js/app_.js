3300742061b6378ae80bbda7e4dedd5a0f17f739
Etah Chen <etahchen@gmail.com>
2015-11-24 01:42:00 -0800

    Update app_.js

--- js/app_.js
+++ js/app_.js
@@ -11,39 +11,54 @@ $(document).ready(function() {
 
 	secretNumGenerator();
 
-//
+//$("body").css("background", "green");
 	function lessGuesses() {
 		if (userGuess===secretNumber) {
 			setFeedback("you win");
+			$("body").css("background-image", "url('https://upload.wikimedia.org/wikipedia/commons/d/d7/Confetti_(5879576562).jpg')");
 			finish = true;
 		} else if ((secretNumber - userGuess) > 65) {
 			setFeedback("I'm freeeezing!");
+			$("body").css("background", "#191970");
 		} else if ((secretNumber - userGuess) > 60) {
 			setFeedback("I need a jacket");
+			$("body").css("background", "#4169E1");
 		} else if ((secretNumber - userGuess) > 55) {
 			setFeedback("It's getting a little chilly");
+			$("body").css("background", "#6495ED");
 		} else if ((secretNumber - userGuess) > 50) {
 			setFeedback("Do you feel a breeze?");
+			$("body").css("background", "#B0C4DE");
 		} else if ((secretNumber - userGuess) > 45) {
-			setFeedback("It's kind of warm in here")
+			setFeedback("It's kind of warm in here");
+			$("body").css("background", "#FFA07A");
 		} else if ((secretNumber - userGuess) > 40) {
 			setFeedback("It's getting hot");
+			$("body").css("background", "#FA8072");
 		} else if ((secretNumber - userGuess) > 35) {
 			setFeedback("I think I'm sweating");
+			$("body").css("background", "#CD5C5C");
 		} else if ((secretNumber - userGuess) > 30) {
 			setFeedback("I'm am sweating!");
+			$("body").css("background", "#DC143C");
 		} else if ((secretNumber - userGuess) > 25) {
 			setFeedback("It's HOT!");
+			$("body").css("background", "#B22222");
 		} else if ((secretNumber - userGuess) > 20) {
 			setFeedback("OUCH! It's super HOT!");
+			$("body").css("background", "#8B0000");
 		} else if ((secretNumber - userGuess) > 15) {
 			setFeedback("FIRE! FIRE! FIRE!");
+			$("body").css("background", "#660000");
 		} else if ((secretNumber - userGuess) > 10) {
 			setFeedback("HOT FIRE!!! HOT FIRE!!!"); 
+			$("body").css("background", "#660000");
 		} else if ((secretNumber - userGuess) > 5) {
 			setFeedback("We're on the SUN!");
+			$("body").css("background", "#330000");
 		} else if ((secretNumber - userGuess) > 0.5) {
 			setFeedback("We're inside the SUN!");
+			$("body").css("background", "#1a0000");
 		} else {
 		}
 	}
@@ -56,32 +71,46 @@ $(document).ready(function() {
 			finish = true;
 		} else if ((userGuess - secretNumber) > 65) {
 			setFeedback("I'm freeeezing!");
+			$("body").css("background", "#191970");
 		} else if ((userGuess - secretNumber) > 60) {
 			setFeedback("I need a jacket");
+			$("body").css("background", "#4169E1");
 		} else if ((userGuess - secretNumber) > 55) {
 			setFeedback("It's getting a little chilly");
+			$("body").css("background", "#6495ED");
 		} else if ((userGuess - secretNumber) > 50) {
 			setFeedback("Do you feel a breeze?");
+			$("body").css("background", "#B0C4DE");
 		} else if ((userGuess - secretNumber) > 45) {
-			setFeedback("It's kind of warm in here")
+			setFeedback("It's kind of warm in here");
+			$("body").css("background", "#FFA07A");
 		} else if ((userGuess - secretNumber) > 40) {
 			setFeedback("It's getting hot");
-		} else if ((userGuess - secretNumber) > 35) {
+			$("body").css("background", "#FA8072");
+			} else if ((userGuess - secretNumber) > 35) {
 			setFeedback("I think I'm sweating");
-		} else if ((userGuess - secretNumber) > 30) {
+			$("body").css("background", "#CD5C5C");
+			} else if ((userGuess - secretNumber) > 30) {
 			setFeedback("I'm am sweating!");
+			$("body").css("background", "#DC143C");
 		} else if ((userGuess - secretNumber) > 25) {
 			setFeedback("It's HOT!");
+			$("body").css("background", "#B22222");
 		} else if ((userGuess - secretNumber) > 20) {
 			setFeedback("OUCH! It's super HOT!");
+			$("body").css("background", "#8B0000");
 		} else if ((userGuess - secretNumber) > 15) {
 			setFeedback("FIRE! FIRE! FIRE!");
+			$("body").css("background", "#660000");
 		} else if ((userGuess - secretNumber) > 10) {
 			setFeedback("HOT FIRE!!! HOT FIRE!!!"); 
+			$("body").css("background", "#4d0000");
 		} else if ((userGuess - secretNumber) > 5) {
 			setFeedback("We're on the SUN!");
+			$("body").css("background", "#330000");
 		} else if ((userGuess - secretNumber) > 0.5) {
 			setFeedback("We're inside the SUN!");
+			$("body").css("background", "#1a0000");
 		} else {
 
 		}
